2633c91c220d6c81f67f5999d55385d6a879dd1d
doel <adoel.cs@gmail.com>
2018-04-12 15:00:53 +0700

    15.00

--- ListFeature__CarWash/feature__v2.js
+++ ListFeature__CarWash/feature__v2.js
@@ -1,43 +1,18 @@
-#
+# ==> DONE
 
-	Scenario: Cerita-nya SuperAdministrator sedang mau Buat User Baru dan memilih Level Akses ==> DONE
+	Scenario: Cerita-nya SuperAdministrator sedang mau Buat User Baru dan memilih Level Akses 
 
 		***BUG_01, ganti "Kasir Member" menjadi "Kasir"
 
-#
+# ==> DONE
 
-	Scenario: Cerita-nya mau Login as SuperAdministrator ==> DONE
+	Scenario: Cerita-nya mau Login as SuperAdministrator 
 
 		***BUG_01, GANTI userlogin "admin" MENJADI "superadministrator"
 
-#
+# "2 hari"
 
-	Scenario: Cerita-nya SuperAdministrator mau Buat Store Baru dan memiliih Kategori
-
-		***BUG_01, Kategori hanya berisi pilihan : 1. Service & 2. F & B
-
-#
-
-	Scenario: Ceritanya SuperAdministrator mau lihat histori Customer telah membeli apa aja (LAPORAN DI SUPERADMINISTRATOR)
-
-		***FEATURE_01, Tambahin "Laporan History", filter berdasarkan "nama customer" || Laporan History Customer
-
-
-#
-
-	Scenario: Cerita-nya "Owner" pengen tahu laporan- laporan yang ada di Store milik-nya (LAPORAN BISA DI PRINT MELALUI PC)
-
-		***FEATURE_01, Laporan barang di jual per-tanggal/ transaksi
-
-		***FEATURE_02, Print list menu 
-
-		***FEATURE_03, Laporan total penjualan per-hari dari tanggal sekian ke sekian
-
-			- Jumlah penjualan perhari.
-
-
-#
-	Scenario: Ceritanya "Kasir/Supervisor" pengen tahu laporan - laporan aktifitas yang terjadi di kasir
+	Scenario: Ceritanya "Kasir/Kasir Member" pengen tahu laporan - laporan aktifitas yang terjadi di kasir
 
 		***FEATURE_01, Laporan top-up harian
 
@@ -49,6 +24,33 @@
 
 		***FEATURE_05, Print when Refund
 
+# "5 hari"
+
+	Scenario: Ceritanya SuperAdministrator mau lihat histori dari setiap "Customer" telah membeli apa aja 
+				di semua store & transaksi (LAPORAN DI SUPERADMINISTRATOR)
+
+		***FEATURE_01, Tambahin "Laporan History", filter berdasarkan "nama customer" || Laporan History Customer
+
+#
+
+	Scenario: Cerita-nya SuperAdministrator mau Buat Store Baru dan memiliih Kategori
+
+		***BUG_01, Kategori hanya berisi pilihan : 1. Service & 2. F & B
+
+# "3 hari"
+
+	Scenario: Cerita-nya "Owner" pengen tahu laporan- laporan yang ada di Store milik-nya (LAPORAN BISA DI PRINT MELALUI PC)
+
+		***FEATURE_01, Laporan barang di jual per-tanggal/ transaksi
+
+			- Apaan aja y
+
+		***FEATURE_02, Print list menu 
+
+		***FEATURE_03, Laporan total penjualan per-hari dari tanggal sekian ke sekian
+
+			- Jumlah penjualan perhari.
+
 
 # 	
 	
@@ -57,7 +59,8 @@
 		***BUG_01, Apa-nya yang tidak harus lengkap ?
 
 
-#	
+#"3hari"
+
 	Scenario : Ceritanya Customer gesek kartu, kemudian nomor seri kartu itu di 'hide'
 
 		***BUG_01, saat ini nomor kartu seri muncul di kolom input 
@@ -67,6 +70,18 @@
 
 		***BUG_01, tambahin nama store di print-out
 
+
+#
+
+	Scenario : Ceritanya Login as Staff "fahmi87654321" dari si owner pengen buat laporan 
+
+		***BUG_01, tampil page Laporan yang isi-nya barang/ transaksi apa aja yang sudah di lakukan di hari itu 
+
+
+	Scenario : Ceritanya Login as SuperAdministrator juga pengen KEPO 
+				aktifitas Si Staff & laporan yang di buat dari suatu Store
+
+		***BUG_01, NOT REQUIREMENT
 
 
 
