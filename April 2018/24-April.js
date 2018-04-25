03518cb7955685e4cf221def32776c48d5322379
doel <adoel.cs@gmail.com>
2018-04-25 15:58:26 +0700

    15.58

---
+++ April 2018/24-April.js
@@ -0,0 +1,260 @@
+{
+	"status": 200,
+	"message": null,
+	"result": {
+		"count": "7",
+		"store": [{
+			"id": 347,
+			"date": "2018-04-24T21:48:33.413Z",
+			"total": 435000,
+			"store": 9,
+			"member": {
+				"id": 6,
+				"name": "David Kurnia Kristiadi",
+				"phone": "08129892015",
+				"email": "davidkurniakristiadi@gmail.com",
+				"address": "Tangerang, BSD, Puspita Loka sektor III.3 blok E.2/10",
+				"balance": 8799950,
+				"payment": null,
+				"card": {
+					"id": "0011509695740738",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 43,
+				"name": "Paket Hemat",
+				"price": "35000",
+				"description": "Menu hemat dengan lauk lengkap",
+				"image": "http://128.199.177.16:3307/public/1522901105569.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 347,
+			"date": "2018-04-24T21:48:33.413Z",
+			"total": 435000,
+			"store": 9,
+			"member": {
+				"id": 6,
+				"name": "David Kurnia Kristiadi",
+				"phone": "08129892015",
+				"email": "davidkurniakristiadi@gmail.com",
+				"address": "Tangerang, BSD, Puspita Loka sektor III.3 blok E.2/10",
+				"balance": 8799950,
+				"payment": null,
+				"card": {
+					"id": "0011509695740738",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 45,
+				"name": "Pizza",
+				"price": "80000",
+				"description": "Pizza dengan size reguler",
+				"image": "http://128.199.177.16:3307/public/1522901586721.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 347,
+			"date": "2018-04-24T21:48:33.413Z",
+			"total": 435000,
+			"store": 9,
+			"member": {
+				"id": 6,
+				"name": "David Kurnia Kristiadi",
+				"phone": "08129892015",
+				"email": "davidkurniakristiadi@gmail.com",
+				"address": "Tangerang, BSD, Puspita Loka sektor III.3 blok E.2/10",
+				"balance": 8799950,
+				"payment": null,
+				"card": {
+					"id": "0011509695740738",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 37,
+				"name": "Hot Dog",
+				"price": "30000",
+				"description": "Dengan daging sapi pilihan",
+				"image": "http://128.199.177.16:3307/public/1522901038361.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 346,
+			"date": "2018-04-24T21:44:20.304Z",
+			"total": 105000,
+			"store": 9,
+			"member": {
+				"id": 226,
+				"name": "Daniel",
+				"phone": null,
+				"email": null,
+				"address": null,
+				"balance": 2939000,
+				"payment": 1,
+				"card": {
+					"id": "0011524030958907",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 36,
+				"name": "Double Burger",
+				"price": "50000",
+				"description": "Double Burger BIG BIG",
+				"image": "http://128.199.177.16:3307/public/1522901643392.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 346,
+			"date": "2018-04-24T21:44:20.304Z",
+			"total": 105000,
+			"store": 9,
+			"member": {
+				"id": 226,
+				"name": "Daniel",
+				"phone": null,
+				"email": null,
+				"address": null,
+				"balance": 2939000,
+				"payment": 1,
+				"card": {
+					"id": "0011524030958907",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 35,
+				"name": "Spageti",
+				"price": "55000",
+				"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+				"image": "http://128.199.177.16:3307/public/1522900439584.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 345,
+			"date": "2018-04-24T21:33:45.098Z",
+			"total": 105000,
+			"store": 9,
+			"member": {
+				"id": 226,
+				"name": "Daniel",
+				"phone": null,
+				"email": null,
+				"address": null,
+				"balance": 2939000,
+				"payment": 1,
+				"card": {
+					"id": "0011524030958907",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 36,
+				"name": "Double Burger",
+				"price": "50000",
+				"description": "Double Burger BIG BIG",
+				"image": "http://128.199.177.16:3307/public/1522901643392.jpeg",
+				"status": true,
+				"category": false
+			}
+		}, {
+			"id": 345,
+			"date": "2018-04-24T21:33:45.098Z",
+			"total": 105000,
+			"store": 9,
+			"member": {
+				"id": 226,
+				"name": "Daniel",
+				"phone": null,
+				"email": null,
+				"address": null,
+				"balance": 2939000,
+				"payment": 1,
+				"card": {
+					"id": "0011524030958907",
+					"type": {
+						"id": 1,
+						"name": "Member",
+						"min": "100000",
+						"bonus": "10000",
+						"refund": false,
+						"charge": false,
+						"status": true
+					}
+				},
+				"status": true
+			},
+			"menu": {
+				"id": 35,
+				"name": "Spageti",
+				"price": "55000",
+				"description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+				"image": "http://128.199.177.16:3307/public/1522900439584.jpeg",
+				"status": true,
+				"category": false
+			}
+		}]
+	}
+}
