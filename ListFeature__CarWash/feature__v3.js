51cd5083c3492b49a1897c30767c3fbc3b26bb14
Andre Rizkiana Wandira <andre.rizkiana.w@gmail.com>
2018-05-01 07:25:38 +0700

    Selasa 1 Mei 2018

--- ListFeature__CarWash/feature__v3.js
+++ ListFeature__CarWash/feature__v3.js
@@ -1,14 +1,55 @@
 
 
+- 0011524969052918 Member 
+
+- Taxi Online 0151513099408336 Jamaludin ==> BERHASIL REFUND
+- Taxi Online 0151513099742807 Yusup
+- Taxi Online 0151513139180314 Andi
+- Taxi Online 0151523603967699 Rachman Hidayat
 
 
 # 
 
 	Feature Kasir 
 
-	Scenario: Berhasil Refund All Member : Taxi Online, Non Member, Member
+	Scenario: Berhasil Refund All Member : Taxi Online, Non Member, Member ==> DONE
 
-	Scenario: Tidak berhasil refund kartu Member
+		***BUG
+		
+		Given, in page Refund Kartu
 
-	Scenario: 
+		When, kasir gesek kartu
+
+		Then, muncul window print-out
+
+		And, tidak muncul Modal Success REFUNDING CARD
+
+		And, tiba- tiba langsung muncul "Maaf, kartu yang Anda gunakan tidak bisa di refund"
+
+		***BUG FIXED ==> DONE
+
+		Given, in page Refund Kartu
+
+		When, kasir gesek kartu
+
+		Then, Muncul Modal Succes REFUNDING 
+
+		And, muncul window print-out
+
+		
+
+
+	Scenario: Member tidak bisa melakukan REFUND 
+
+		Given, in page Refund Kartu
+
+		When, kasir gesek kartu
+
+		And, Muncul Modal Keterangan Member, dll
+
+		And, Kasir Klik Button Refund
+
+		Then, Muncul Keterangan "Maaf, kartu yang Anda gunakan tidak bisa di refund"
+
+
 
