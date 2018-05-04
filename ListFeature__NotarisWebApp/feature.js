a60b10a5b793b378f37d5d1d2668bd8c82a5078e
doel <adoel.cs@gmail.com>
2018-05-04 20:00:09 +0700

    20.00

---
+++ ListFeature__NotarisWebApp/feature.js
@@ -0,0 +1,9 @@
+# 
+
+	- Feature Notaris Web PApp 
+
+	Scenario: Direktur Notaris Mengkses Page Login Notaris Web App
+
+		
+	
+	Scenario: Direktur Notaris Berhasil Login Dan masuk ke tampilan Page Beranda Direktur Notaris
