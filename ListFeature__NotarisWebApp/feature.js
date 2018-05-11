586e07e66824e14135e87b82e0209a63f98d0c15
doel <adoel.cs@gmail.com>
2018-05-11 18:43:26 +0700

    18.43

--- ListFeature__NotarisWebApp/feature.js
+++ ListFeature__NotarisWebApp/feature.js
@@ -1,31 +1,47 @@
-# 
-
-	- Feature Notaris Web PApp 
-
-	Scenario: Direktur Notaris Mengkses Page Login Notaris Web App
-
-	
-	Scenario: Direktur Notaris Berhasil Login Dan masuk ke tampilan Page Beranda Direktur Notaris
-
-
 
 # Tentang Feature Document 
 
+	- Feature hover tooltip untuk semua features 
+
+	- Feature Undo
+
+	- Fitur Redo
+
+	- Fitur 'Teks Normal' ==> DONE
+
+	- Feature Font Style
+
+	- Feature Font Size
+
+	- Feature Bold
+
+	- Feature Italic
+
+	- Feature Underscore
+
+	- Feature Select color
+
+	- Feature sisipkan gambar
+
+	- Feature Rata Tengah - Kiri
+
+	- Feature Jarak Antar Baris (Line Spacing)
+
+	- Feature List Number
+
+	- Feature List Bullet
+
 	- Feature Tabs ==> DONE
-
-	- Feature Line Spacing
-
+	
 	- Feature replace dash to replace empty space
+
 
 	- Feature Print
 
 	- Feature Export to PDF
 
-	- Feature Stylesheet 
-
 	- Feature Button Save
 
-	- Feature Rata Tengah - Kiri
 
 	- Feature A4 
 
@@ -34,6 +50,20 @@
 	- 
 
 	- REFERENSI FEATURE ==> https://docs.google.com/document/d/1jxvps6GYVlMuYtrVu6WLYX2tCUxvngsh5uXHQ7xOY8E/edit
+
+
+# Tentang Feature Lawyer Web Application
+	
+	- Feature Login Functional :
+		
+		Scenario: Direktur Notaris Mengkses Page Login Notaris Web App
+
+		Scenario: Direktur Notaris Berhasil Login Dan masuk ke tampilan Page Beranda Direktur Notaris
+
+
+	- Feature Login Interface
+
+
 
 
 
