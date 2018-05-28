3dfc6524ab2ac7bc1e6871c84c5f495ef8be50d7
Andre Rizkiana Wandira <andre.rizkiana.w@gmail.com>
2018-05-28 09:13:24 +0700

    temp 09.13

---
+++ ListFeature__CarWash/feature__v4.js
@@ -0,0 +1,47 @@
+********************************Paket Gold********************************
+
+# 
+
+	Feature Bisnis proses Hak Akses
+
+	Scenario: Level Superadmin dapat mengakses hingga level akses terbawah
+
+				- Level akses Superadmin
+
+				- Level Akses Kasir
+
+				- Level Akses Owner
+
+				- Level Akses Staff Owner
+
+
+
+#
+
+	Feature Customer
+
+	Scenario: Hak Akses Kasir terhadap Customer
+
+
+#
+
+	Feature Kasir
+
+	Scenario: Customer dapat melakukan pengisian ulang/ Top Up terhadap kartu mereka
+
+	Scenario: Customer baru dapat membuat kartu baru
+
+	Scenario: Customer Non Member dapat mengembalikan uang-nya
+
+	Scenario: Kasir membuat laporan
+
+
+#
+	
+	Feature Owner Staff
+
+
+#
+	
+	Feature Staff
+	
