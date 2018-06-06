e25b290c3b050ddf6fe1a3fc29549393a268a95f
doel <adoel.cs@gmail.com>
2018-06-06 16:02:21 +0700

    16.02

--- ListFeature__CarWash/feature__v5.js
+++ ListFeature__CarWash/feature__v5.js
@@ -2,7 +2,7 @@
 
 	Feature Kartu Baru New
 
-	Scenario: Berhasil membuat kartu baru dengan menggunakan seri number card of Stock Kartu 
+	Scenario: Berhasil membuat kartu baru dengan menggunakan seri number card of Stock Kartu ==> DONE
 		
 		Given, in page Kartu Baru
 
@@ -11,12 +11,45 @@
 		Then, muncul Modal 
 
 
-	Scenario: Muncul Modal tampilan Member
+	Scenario: Muncul Modal tampilan Member ==> DONE
 
 		Given, in Modal inside page Kartu Baru
 
-		When, Modal khusus tipe Member muncul
+		When, Modal khusus tipe "Member" muncul
 
 		Then, kasir melengkapi kolom yang tersedia
+
+	
+	Scenario: Berhasil print saat kartu Member di buat ==> DONE
+		
+		Given, in Modal inside page Kartu Baru
+
+		When, kasir melengkapi kolom yang tersedia
+
+		And, klik button Simpan 
+
+		Then, muncul pop-up Printing
+
+	
+	Scenario: Muncul Modal tampilan Taxi Online ==> DONE
+
+	Scenario: Muncul Modal tampilan Non-Member ==> DONE
+
+
+#
+
+	 Feature Stock Kartu
+
+	 Scenario: Berhasil berubah warna saat seri number berhasil di copy dan di rebuild.
+
+	 	Given, in page Stock Kartu
+
+	 	When, klik button "Copy Nomor Kartu"
+
+	 	Then, warna button berubah menjadi orange
+
+	 	And, tulisan di dalam button menjadi "Copied"
+
+	 	And, fungsi copy to clipboard berjalan.
 
 	
