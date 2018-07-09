c8eea396984e9fc9f7c72cef4714fb508f744665
doel <adoel.cs@gmail.com>
2018-07-09 19:05:40 +0700

    19.05

---
+++ ListFeature__CarWash/features__v7.js
@@ -0,0 +1,11 @@
+#Feature Level Akses Superadmin
+	
+	- Scenario: xxx
+		
+		Given, in page Manajemen Kartu - Daftar Kartu
+			
+			***Ganti jadi "Daftar Member Baru"
+
+
+
+	- Scenario: "laporan di owner -- ganti jadi laporan harian" ==> DONE
